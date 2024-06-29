a51e65c741c323eb416cb89363b4ccdbe38a33e7
unknown <duyzfclong@gmail.com>
2024-06-29 17:42:57 +0700

    fix loading

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 import clsx from 'clsx';
 
-interface IButton2 {
+interface IButton {
     type?: "primary" | "ghost" | "secondary"
     text: string,
     isDisabled?: boolean,
@@ -17,11 +17,17 @@ const Button = ({
     isLoading = false,
     icon,
     onClick
-}: IButton2) => {
+}: IButton) => {
     const typeClass = {
         primary: 'bg-primary-500 text-white',
         ghost: ' text-primary-500 bg-primary-50',
         secondary: 'text-gray-400 border border-gray-400',
+    };
+
+    const typeLoading = {
+        primary: 'border-white border-t-primary-500',
+        ghost: 'border-primary-500 border-t-primary-50',
+        secondary: 'border-gray-400 border-t-white',
     };
 
     return (
@@ -30,17 +36,17 @@ const Button = ({
                 if (onClick && !isDisabled && !isLoading) onClick()
             }}
             className={clsx(
-                'px-[14px] py-[10px] rounded-[8px] text-m-semibold  flex items-center justify-center gap-1',
+                'px-[14px] py-[10px] rounded-[8px] text-m-semibold transition-colors  flex items-center justify-center gap-1',
                 typeClass[type],
                 {
-                    'opacity-65 cursor-not-allowed': isDisabled || isLoading,
+                    'opacity-65 cursor-not-allowed': isDisabled,
+                    'opacity-65': isLoading,
                     'hover:opacity-80': !isDisabled && !isLoading
                 }
             )}
-            disabled={isDisabled || isLoading}
         >
             {isLoading ?
-                <div className="border-gray-300 h-4 w-4 animate-spin rounded-full border-2 border-t-black-500" /> : icon
+                <div className={clsx(`${typeLoading[type]} h-4 w-4 animate-spin rounded-full border-2 `)} /> : icon
             }
             {text}
         </button>
