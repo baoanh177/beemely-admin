87b8d32937a0777181aaefae16606e940a9b4043
unknown <duyzfclong@gmail.com>
2024-07-01 02:09:05 +0700

    update

--- src/layouts/Default/index.tsx
+++ src/layouts/Default/index.tsx
@@ -2,12 +2,10 @@ import { Outlet } from "react-router-dom";
 
 import Sidebar from "./Sidebar";
 import TopBar from "./TopBar";
-import UpdateImage from "@/components/UploadImage";
 const DefaultLayout = () => {
   return (
     <Sidebar>
       <TopBar />
-      <UpdateImage />
       <Outlet />
     </Sidebar>
   );
--- src/pages/InputTextNumber/InputTextNumber.tsx
+++ src/pages/InputTextNumber/InputTextNumber.tsx
@@ -3,7 +3,11 @@ import FormInput from "@/components/form/FormInput"
 const InputTextNumber = () => {
   return (
     <div>
-      <FormInput error="dsfsdf" placeholder="hahaha" type="number"/>
+      <FormInput placeholder="Đây là input number" type="number" />
+      <FormInput placeholder="Đây là input number bị disabled" isDisabled type="number" />
+      <FormInput placeholder="Đây là input text" type="text" />
+      <FormInput error="error!!!" placeholder="Đây là input text bị lỗi" type="text" />
+      <FormInput placeholder="Đây là input text bị readonly" isReadonly type="text" />
     </div>
   )
 }
