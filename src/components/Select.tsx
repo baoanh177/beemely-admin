26a501c8cdb06778a683f0431a5b8333a6ad3dd7
unknown <duyzfclong@gmail.com>
2024-06-30 15:49:56 +0700

    feat/input-single,multi-select

---
+++ src/components/Select.tsx
@@ -0,0 +1,49 @@
+import { ConfigProvider, Select } from "antd";
+import clsx from "clsx";
+interface ICustomSelect {
+    label?: string;
+    placeholder?: string;
+    options?: { value: string; label: string }[];
+    defaultValue?: string | string[];
+    isMultiple?: boolean;
+    onChange?: (value: string | string[]) => void;
+}
+
+const CustomSelect = ({
+    label,
+    placeholder,
+    options,
+    defaultValue,
+    isMultiple,
+    onChange,
+}: ICustomSelect) => (
+    <ConfigProvider
+        theme={{
+            components: {
+                Select: {
+                    colorIconHover: "#883DCF",
+                    colorIcon: "#883DCF",
+                    multipleItemHeight: 33
+
+                },
+            },
+        }}
+    >
+        <div className="mb-1 text-black-300 text-sm font-medium">{label}</div>
+        <Select
+            className={clsx(isMultiple && "text-primary-400", "min-h-[40px] w-[240px]")}
+            mode={isMultiple ? "multiple" : undefined}
+            defaultValue={defaultValue}
+            onChange={onChange}
+            showSearch
+            placeholder={placeholder}
+            optionFilterProp="label"
+            filterSort={(optionA, optionB) =>
+                (optionA?.label ?? "").toLowerCase().localeCompare((optionB?.label ?? "").toLowerCase())
+            }
+            options={options}
+        />
+    </ConfigProvider>
+);
+
+export default CustomSelect;
