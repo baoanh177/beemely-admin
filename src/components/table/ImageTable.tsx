62d984cd5425451e2ec573fa0f93b909ad062831
nguyenthihien <hienn78333@gmail.com>
2024-07-04 20:38:35 +0700

    fix/css-image-table

--- src/components/table/ImageTable.tsx
+++ src/components/table/ImageTable.tsx
@@ -34,7 +34,7 @@ const ImageTable: React.FC<ImageTableProps> = ({ imageSrc, title, description })
             <Avatar src={imageSrc} onError={handleImageError} className="flex h-11 w-11 items-center justify-center overflow-hidden rounded-lg" />
           )
         }
-        title={<Text className="text-m-regular">{title}</Text>}
+        title={<Text className="text-m-medium">{title}</Text>}
         description={
           <Text className="text-s-regular text-gray-500" type="secondary">
             {description}
