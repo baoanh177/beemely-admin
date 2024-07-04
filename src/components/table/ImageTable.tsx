5d6e48b6f29f5fcd392b85afe0ef91be3e3db760
nguyenthihien <hienn78333@gmail.com>
2024-07-05 01:36:55 +0700

    fix/imagempt

--- src/components/table/ImageTable.tsx
+++ src/components/table/ImageTable.tsx
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from "react";
 import imageError from "@/assets/images/imgError-table.jpg";
+import imgEmpty from "@/assets/images/image-empty.png";
 
 interface ImageTableProps {
-  imageSrc: string;
+  imageSrc?: string;
   title: string;
   description?: string;
 }
 
 const ImageTable: React.FC<ImageTableProps> = ({ imageSrc, title, description }) => {
-  const [imgSrc, setImgSrc] = useState(imageSrc);
+  const [imgSrc, setImgSrc] = useState<string | undefined>(undefined);
 
   useEffect(() => {
-    if (!imageSrc) {
-      setImgSrc(imageError);
+    if (!imageSrc || imageSrc.trim() === "") {
+      setImgSrc(imgEmpty);
     } else {
       setImgSrc(imageSrc);
     }
@@ -24,7 +25,13 @@ const ImageTable: React.FC<ImageTableProps> = ({ imageSrc, title, description })
 
   return (
     <div className="flex items-center gap-2">
-      <img src={imgSrc} onError={handleError} className="h-11 w-11 rounded-lg bg-gray-50 object-cover" alt="" />
+      {typeof imgSrc !== "undefined" ? (
+        <img src={imgSrc} onError={handleError} className="h-11 w-11 rounded-lg bg-gray-50 object-cover" alt="" />
+      ) : (
+        <div className="flex h-11 w-11 items-center justify-center rounded-lg bg-gray-50">
+          <span className="text-m-medium text-gray-400">{title[0]}</span>
+        </div>
+      )}
       <div className="flex flex-col gap-1">
         <div className="text-m-medium text-black-500">{title}</div>
         <div className="text-m-medium text-gray-500">{description}</div>
