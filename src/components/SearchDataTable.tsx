f479d96193778adb479a43c3d881dfcae34652ab
unknown <duyzfclong@gmail.com>
2024-07-05 01:30:34 +0700

    format code

--- src/components/SearchDataTable.tsx
+++ src/components/SearchDataTable.tsx
@@ -2,39 +2,36 @@ import React, { useState } from "react";
 import { ConfigProvider, Segmented } from "antd";
 
 interface Option {
-    label: string;
-    value: string;
+  label: string;
+  value: string;
 }
 
 interface Props {
-    options: Option[];
+  options: Option[];
 }
 
 const SearchDataTable: React.FC<Props> = ({ options }) => {
-    const [selectedValue, setSelectedValue] = useState<string>(options[0]?.label);
-    const handleChange = (value: string) => {
-        setSelectedValue(value);
-        selectedValue
-    };
-    return (
-        <div>
-            <ConfigProvider
-                theme={{
-                    components: {
-                        Segmented: {
-                            motionDurationMid: "0.01s",
-                            motionDurationSlow: "0.01s",
-                        },
-                    },
-                }}
-            >
-                <Segmented
-                    onChange={handleChange}
-                    options={options.map((option) => option.label)}
-                />
-            </ConfigProvider>
-        </div>
-    );
+  const [selectedValue, setSelectedValue] = useState<string>(options[0]?.label);
+  const handleChange = (value: string) => {
+    setSelectedValue(value);
+    selectedValue;
+  };
+  return (
+    <div>
+      <ConfigProvider
+        theme={{
+          components: {
+            Segmented: {
+              motionDurationMid: "0.01s",
+              motionDurationSlow: "0.01s",
+            },
+          },
+        }}
+      >
+        <Segmented onChange={handleChange} options={options.map((option) => option.label)} />
+      </ConfigProvider>
+    </div>
+  );
 };
 
 export default SearchDataTable;
