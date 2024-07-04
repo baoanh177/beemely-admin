a4006ee1a91cf4ba1fcab65d7302ae4aba6a6a22
unknown <duyzfclong@gmail.com>
2024-07-05 01:25:02 +0700

    fix options received from props

--- src/components/SearchDataTable.tsx
+++ src/components/SearchDataTable.tsx
@@ -2,38 +2,39 @@ import React, { useState } from "react";
 import { ConfigProvider, Segmented } from "antd";
 
 interface Option {
-  label: string;
-  value: string;
+    label: string;
+    value: string;
 }
 
-const options: Option[] = [
-  { label: "Product All", value: "123" },
-  { label: "Published", value: "234" },
-  { label: "Low Stock", value: "123" },
-  { label: "Draft", value: "123" },
-];
-const SearchDataTable: React.FC = () => {
-  const [selectedValue, setSelectedValue] = useState<string>("Product All");
-  const handleChange = (value: string) => {
-    setSelectedValue(value);
-    selectedValue;
-  };
-  return (
-    <div>
-      <ConfigProvider
-        theme={{
-          components: {
-            Segmented: {
-              motionDurationMid: "0.01s",
-              motionDurationSlow: "0.01s",
-            },
-          },
-        }}
-      >
-        <Segmented onChange={handleChange} options={options.map((option) => option.label)} />
-      </ConfigProvider>
-    </div>
-  );
+interface Props {
+    options: Option[];
+}
+
+const SearchDataTable: React.FC<Props> = ({ options }) => {
+    const [selectedValue, setSelectedValue] = useState<string>(options[0]?.label);
+    const handleChange = (value: string) => {
+        setSelectedValue(value);
+        selectedValue
+    };
+    return (
+        <div>
+            <ConfigProvider
+                theme={{
+                    components: {
+                        Segmented: {
+                            motionDurationMid: "0.01s",
+                            motionDurationSlow: "0.01s",
+                        },
+                    },
+                }}
+            >
+                <Segmented
+                    onChange={handleChange}
+                    options={options.map((option) => option.label)}
+                />
+            </ConfigProvider>
+        </div>
+    );
 };
 
 export default SearchDataTable;
--- src/components/table/PrimaryTable.tsx
+++ src/components/table/PrimaryTable.tsx
@@ -7,37 +7,42 @@ import FormDate from "../form/FormDate";
 import { IoSearchOutline } from "react-icons/io5";
 
 export interface DataType {
-  key: React.Key;
-  [key: string]: unknown;
+    key: React.Key;
+    [key: string]: unknown;
 }
 
 interface IPrimaryTableProps {
-  search: false | { status: { value: string; label: string }[] };
-  columns: ColumnsType<DataType>;
-  data: DataType[];
-  pagination?: { pageSize: number; current: number; total: number };
+    search: false | { status: { value: string; label: string }[] };
+    columns: ColumnsType<DataType>;
+    data: DataType[];
+    pagination?: { pageSize: number; current: number; total: number };
 }
 
 const onSelectChange = (selectedRowKeys: React.Key[], selectedRows: DataType[]) => {
-  selectedRowKeys;
-  selectedRows;
+    selectedRowKeys;
+    selectedRows;
 };
 
 const PrimaryTable: React.FC<IPrimaryTableProps> = ({ search, columns, data, pagination }) => {
-  return (
-    <div className="flex flex-col gap-6">
-      {search && (
-        <div className="flex justify-between">
-          <SearchDataTable />
-          <div className="flex gap-4">
-            <FormInput icon={IoSearchOutline} placeholder="Search product. . ." type="text" />
-            <FormDate />
-          </div>
+    return (
+        <div className="flex flex-col gap-6">
+            {search && (
+                <div className="flex justify-between">
+                    <SearchDataTable options={[
+                        { value: "1", label: "All Product" },
+                        { value: "2", label: "Published" },
+                        { value: "3", label: "Draft" },
+                        { value: "4", label: "Low Stock" },
+                    ]} />
+                    <div className="flex gap-4">
+                        <FormInput icon={IoSearchOutline} placeholder="Search product. . ." type="text" />
+                        <FormDate />
+                    </div>
+                </div>
+            )}
+            <Table rowSelection={{ onChange: onSelectChange }} columns={columns} dataSource={data} pagination={pagination && pagination} />
         </div>
-      )}
-      <Table rowSelection={{ onChange: onSelectChange }} columns={columns} dataSource={data} pagination={pagination && pagination} />
-    </div>
-  );
+    );
 };
 
 export default PrimaryTable;
