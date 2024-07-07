58963ea9628e8670cd82171578585b6fa52e36a4
nguyenthihien <hienn78333@gmail.com>
2024-07-07 11:37:24 +0700

    feat/card-customer

---
+++ src/components/CardCustomer.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import StatusBadge from "./table/StatusBadge";
+interface StatusType {
+  text: string;
+  color: "green" | "red";
+}
+interface CustomerCardProps {
+  image: string;
+  title: string;
+  status: StatusType;
+  orders: number;
+  balance: number;
+}
+
+export const customerData = [
+  {
+    image: "https://picsum.photos/200/300",
+    title: "Laura Prichet",
+    status: { text: "Blocked", color: "red" as "red" | "green" },
+    orders: 23434,
+    balance: 23434,
+  },
+  {
+    image: "https://picsum.photos/200/300",
+    title: "John Doe",
+    status: { text: "Active", color: "green" as "red" | "green" },
+    orders: 10000,
+    balance: 5000,
+  },
+];
+const CustomerCard: React.FC<CustomerCardProps> = ({ image, title, status, orders, balance }) => {
+  return (
+    <div className="flex max-w-[216px] flex-col gap-8 rounded-xl bg-white p-4">
+      <div className="flex flex-1 flex-col items-center gap-4">
+        <div className="relative flex w-full items-center justify-center">
+          <div>
+            <img src={image} className="h-20 w-20 rounded-circle object-cover" alt={title} />
+          </div>
+          <div className="absolute right-0 top-0 text-gray-400">
+            <BsThreeDotsVertical />
+          </div>
+        </div>
+        <div className="flex h-12 w-full flex-col items-center gap-1">
+          <div className="text-m-semibold">{title}</div>
+          <StatusBadge text={status.text} color={status.color} />
+        </div>
+      </div>
+      <hr className="border-dashed"></hr>
+      <div className="flex h-[42px] w-full items-center justify-between gap-[10px]">
+        <div className="flex w-full flex-col items-center justify-center gap-1">
+          <span className="text-s-regular text-gray-500">Orders</span>
+          <span className="text-m-medium">{orders}</span>
+        </div>
+        <div className="flex w-full flex-col items-center justify-center gap-1">
+          <span className="text-s-regular text-gray-500">Balance</span>
+          <span className="text-m-medium">${balance}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CustomerCard;
