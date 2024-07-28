0756cf8a1dd9c75819760a6eefae348fbcbe3ca2
nguyenthihien <hienn78333@gmail.com>
2024-07-28 10:35:01 +0700

    .

---
+++ src/hooks/useAsyncEffect.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+const useAsyncEffect = (callback: (async: Function) => unknown, dependencies: unknown[] = []) => {
+  const [loading, setLoading] = useState<Record<string, boolean>>({});
+
+  const async = async (promise: Promise<unknown>, key: string): Promise<unknown> => {
+    setLoading((prev) => ({ ...prev, [key]: true }));
+    return promise
+      .finally(() => {
+        setLoading((prev) => ({ ...prev, [key]: false }));
+      });
+  };
+
+  useEffect(() => {
+    callback(async);
+  }, dependencies);
+
+  return loading
+};
+
+export default useAsyncEffect;
