c748b88b8f1e7455dea30b5c3729fb900e4a66bc
baoanh <baoanh27042004@gmail.com>
2024-07-29 20:01:28 +0700

    responsive<baoanh>: topbar & pagination

--- src/components/common/PaginationText.tsx
+++ src/components/common/PaginationText.tsx
@@ -1,4 +1,4 @@
 const PaginationText = (total: number, range: [number, number]) => {
-  return <div className="hidden md:block">{`Hiển thị ${range[0]}-${range[1]} từ ${total}`}</div>
+  return <div className="hidden md:block">{`Hiển thị ${range[0]}-${range[1]} từ ${total}`}</div>;
 };
 export default PaginationText;
