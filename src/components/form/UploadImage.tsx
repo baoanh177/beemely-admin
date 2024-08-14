50d6413f50310000debd87d7b604616bd1c8ab2a
nguyenthihien <hienn78333@gmail.com>
2024-08-15 05:23:13 +0700

    feat<hien>:loading-updateImage

--- src/components/form/UploadImage.tsx
+++ src/components/form/UploadImage.tsx
@@ -1,8 +1,8 @@
-import { message } from "antd";
+import { message, Spin } from "antd";
 import imageError from "@/assets/images/imgError.jpg";
 import React, { useState, useEffect } from "react";
 import { IoIosCloseCircle } from "react-icons/io";
-
+import { LoadingOutlined } from "@ant-design/icons";
 interface UploadImageProps {
   isMultiple?: boolean;
   label: string;
@@ -14,6 +14,7 @@ const UploadImage: React.FC<UploadImageProps> = ({ isMultiple = false, label, on
   const [fileList, setFileList] = useState<File[]>([]);
   const [uploadedImageUrls, setUploadedImageUrls] = useState<string[]>(currentImageUrl ? [currentImageUrl] : []);
   const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
     if (currentImageUrl) {
@@ -30,13 +31,21 @@ const UploadImage: React.FC<UploadImageProps> = ({ isMultiple = false, label, on
     setUploadedImageUrls(updatedImageUrls);
     onImageUpload?.(isMultiple ? updatedImageUrls : updatedImageUrls[0] || "");
   };
-
+  const preloadImage = (src: string) =>
+    new Promise((resolve, reject) => {
+      const img = new Image();
+      img.onload = () => resolve(src);
+      img.onerror = reject;
+      img.src = src;
+    });
   const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
     if (files) {
       const newFiles: File[] = Array.from(files);
+      setIsLoading(true);
       try {
         const imageUrls = await Promise.all(newFiles.map((file) => uploadImage(file)));
+        await Promise.all(imageUrls.map(preloadImage));
         if (isMultiple) {
           const allImageUrls = [...uploadedImageUrls, ...imageUrls];
           setUploadedImageUrls(allImageUrls);
@@ -49,6 +58,8 @@ const UploadImage: React.FC<UploadImageProps> = ({ isMultiple = false, label, on
       } catch (error) {
         message.error("Error uploading files, please try again");
         setError("Error uploading files, please try again");
+      } finally {
+        setIsLoading(false);
       }
     }
   };
@@ -95,32 +106,36 @@ const UploadImage: React.FC<UploadImageProps> = ({ isMultiple = false, label, on
   return (
     <div>
       <div>{label}</div>
-      <div className="custom-upload flex h-[260px] items-center justify-center rounded-lg bg-gray-25 px-3 py-6">
-        <div className="flex-col items-center gap-4">
-          <div className="flex flex-wrap justify-center">
-            {uploadedImageUrls.length > 0
-              ? uploadedImageUrls.map((url, index) => (
-                  <div key={index} className="relative mx-2 inline-block text-center">
-                    {renderFileIcon(url)}
-                    <button onClick={() => handleDeleteImage(fileList[index]?.name || "")}>
-                      <IoIosCloseCircle className="absolute right-1 top-1 h-[24px] w-[24px] rounded-circle text-green-100" />
-                    </button>
-                  </div>
-                ))
-              : renderDefaultContent()}
-          </div>
-          {error && <div className="mt-3 text-center text-red-500">{error}</div>}
-          <div className="mt-4 flex justify-center">
-            <label
-              htmlFor="file-upload"
-              className="text-m-medium inline-block cursor-pointer rounded bg-primary-50 px-[14px] py-[10px] text-primary-500"
-            >
-              {isMultiple ? "Thêm ảnh" : uploadedImageUrls.length > 0 ? "Thay đổi ảnh" : "Thêm ảnh"}
-            </label>
-            <input id="file-upload" type="file" onChange={handleFileChange} multiple={isMultiple} className="hidden" />
+      <Spin indicator={<LoadingOutlined spin />} className="text-primary-300" size="large" spinning={isLoading}>
+        <div className="custom-upload flex h-[260px] items-center justify-center rounded-lg bg-gray-25 px-3 py-6">
+          <div className="flex-col items-center gap-4">
+            {
+              <div className="flex flex-wrap justify-center">
+                {uploadedImageUrls.length > 0
+                  ? uploadedImageUrls.map((url, index) => (
+                      <div key={index} className="relative mx-2 inline-block text-center">
+                        {renderFileIcon(url)}
+                        <button onClick={() => handleDeleteImage(fileList[index]?.name || "")}>
+                          <IoIosCloseCircle className="absolute right-1 top-1 h-[24px] w-[24px] rounded-circle text-green-100" />
+                        </button>
+                      </div>
+                    ))
+                  : renderDefaultContent()}
+              </div>
+            }
+            {error && <div className="mt-3 text-center text-red-500">{error}</div>}
+            <div className="mt-4 flex justify-center">
+              <label
+                htmlFor="file-upload"
+                className="text-m-medium inline-block cursor-pointer rounded bg-primary-50 px-[14px] py-[10px] text-primary-500"
+              >
+                {isMultiple ? "Thêm ảnh" : uploadedImageUrls.length > 0 ? "Thay đổi ảnh" : "Thêm ảnh"}
+              </label>
+              <input id="file-upload" type="file" onChange={handleFileChange} multiple={isMultiple} className="hidden" />
+            </div>
           </div>
         </div>
-      </div>
+      </Spin>
     </div>
   );
 };
