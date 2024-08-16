5d259d5cfc2ca2f336ef590e7354e934a81e1759
unknown <duyzfclong@gmail.com>
2024-08-16 22:35:28 +0700

    fix<long>: voucherTypes module - add detail & change voucherTypes list

--- src/routes/voucher.route.tsx
+++ src/routes/voucher.route.tsx
@@ -2,6 +2,7 @@ import { IRoute } from "./routes";
 import Vouchers from "@/pages/Voucher/Vouchers/Vouchers";
 import CreateVoucher from "@/pages/Voucher/CreateVoucher/CreateVoucher";
 import UpdateVoucher from "@/pages/Voucher/UpdateVoucher/UpdateVoucher";
+import DetailVoucher from "@/pages/Voucher/DetailVoucher/DetailVoucher";
 
 export const voucherRoutes: IRoute[] = [
   {
@@ -11,6 +12,10 @@ export const voucherRoutes: IRoute[] = [
   {
     path: "/create",
     element: () => <CreateVoucher />,
+  },
+  {
+    path: "/detail/:id",
+    element: () => <DetailVoucher />,
   },
   {
     path: "/update/:id",
