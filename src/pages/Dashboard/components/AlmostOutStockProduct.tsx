bc6bec5ac19725b46236fbcb97eadc81cae94b34
Xuan Tho <tholx2811workspace@gmail.com>
2024-11-22 17:11:02 +0700

    added stat

---
+++ src/pages/Dashboard/components/AlmostOutStockProduct.tsx
@@ -0,0 +1,18 @@
+import { useArchive } from "@/hooks/useArchive";
+import useAsyncEffect from "@/hooks/useAsyncEffect";
+import { IStatsInitialState } from "@/services/store/stats/stats.slice";
+import { getAlmostOutStockProduct } from "@/services/store/stats/stats.thunk";
+import React from "react";
+
+const AlmostOutStockProduct = () => {
+  const { state, dispatch } = useArchive<IStatsInitialState>("stats");
+
+  useAsyncEffect(
+    (async) => async(dispatch(getAlmostOutStockProduct({ query: { _pagination: false, ...state.filter } })), "getMostPurchasedColor"),
+    [JSON.stringify(state.filter)],
+  );
+  console.log(state);
+  return <div>AlmostOutStockProduct</div>;
+};
+
+export default AlmostOutStockProduct;
